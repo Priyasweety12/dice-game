5993f4237b36f8f31a9b1ca5a27b2141d2360fcd
Priyasweety12 <53477926+Priyasweety12@users.noreply.github.com>
2019-08-29 14:35:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+// Generating first random number
+var randomNum1=Math.floor(Math.random()*6)+1
+// Changing the image based on the random number
+document.getElementsByTagName("img")[0].setAttribute("src", "images/dice" +randomNum1+ ".png");
+// Generating second random number
+var randomNum2=Math.floor(Math.random()*6)+1
+// Changing the image based on the random number
+document.getElementsByTagName("img")[1].setAttribute("src", "images/dice" +randomNum2+ ".png");
+
+// Checking which player won the Game
+if(randomNum1>randomNum2){
+  document.querySelector("h1").innerHTML="🚩Player 1 wins!";
+}
+else if(randomNum1<randomNum2){
+  document.querySelector("h1").innerHTML="Player 2 Wins!🚩";
+}
+else{
+  document.querySelector("h1").innerHTML="Draw!";
+}
