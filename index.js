885934da957cc66decf8e92dad483dfa6c5a8211
Priyasweety12 <53477926+Priyasweety12@users.noreply.github.com>
2019-08-29 14:36:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Generating first random number
 var randomNum1=Math.floor(Math.random()*6)+1
 // Changing the image based on the random number
-document.getElementsByTagName("img")[0].setAttribute("src", "images/dice" +randomNum1+ ".png");
+document.getElementsByTagName("img")[0].setAttribute("src", "dice" +randomNum1+ ".png");
 // Generating second random number
 var randomNum2=Math.floor(Math.random()*6)+1
 // Changing the image based on the random number
-document.getElementsByTagName("img")[1].setAttribute("src", "images/dice" +randomNum2+ ".png");
+document.getElementsByTagName("img")[1].setAttribute("src", "dice" +randomNum2+ ".png");
 
 // Checking which player won the Game
 if(randomNum1>randomNum2){
